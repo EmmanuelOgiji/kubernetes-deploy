8e1b95a44b466f4b52969561d891b1db1a522cc4
EmmanuelOgiji <epiusogiji@yahoo.com>
2021-08-11 17:25:12 +0100

    Get nodes to deploy: - Add public subnet nodes

--- eks.tf
+++ eks.tf
@@ -46,3 +46,29 @@ resource "aws_eks_node_group" "deployment_nodes" {
     aws_iam_role_policy_attachment.node_role-cluster_autoscaler,
   ]
 }
+
+resource "aws_eks_node_group" "deployment_nodes_public" {
+  cluster_name    = aws_eks_cluster.deployment.name
+  node_group_name = "deployment_nodes_public"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = aws_subnet.public.*.id
+  instance_types  = ["t2.micro"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  tags = var.standard_tags
+
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.node_role-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node_role-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node_role-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.node_role-cluster_autoscaler,
+  ]
+}
